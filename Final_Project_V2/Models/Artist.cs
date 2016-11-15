ff17bac578d97489e7b19baae5758aaef7138984
anishaas <anisha.srivastava123@gmail.com>
2016-11-14 23:22:33 -0600

    added properties to artist model class

--- Final_Project_V2/Models/Artist.cs
+++ Final_Project_V2/Models/Artist.cs
@@ -7,5 +7,13 @@ namespace Final_Project_V2.Models
 {
     public class Artist
     {
+        public Int32 ArtistID { get; set; }
+        public string ArtistName { get; set; }
+        public DateTime ArtistTimeStamp { get; set; }
+
+        //navigational
+        public virtual List<Genre> ArtistGenres { get; set; }
+        public virtual List<Album> ArtistAlbums { get; set; }
+        public virtual List<Song> ArtistSongs { get; set; }
     }
 }
