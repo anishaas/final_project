8e4bec6c87dc87070bb109dcd9fc1390c83513c7
anishaas <anisha.srivastava123@gmail.com>
2016-11-14 23:27:41 -0600

    Added properties to song model class

--- Final_Project_V2/Models/Genre.cs
+++ Final_Project_V2/Models/Genre.cs
@@ -7,8 +7,12 @@ namespace Final_Project_V2.Models
 {
     public class Genre
     {
+        public Int32 GenreID { get; set; }
+        public DateTime GenreTimestamp { get; set; }
 
+        public virtual List<Album> GenreAlbums { get; set; }
+        public virtual List<Song> GenreSongs { get; set; }
+        public virtual List<Artist> GenreArtists { get; set; }
 
-        public virtual List<Album> Albums { get; set; }
     }
 }
--- Final_Project_V2/Models/Song.cs
+++ Final_Project_V2/Models/Song.cs
@@ -7,5 +7,15 @@ namespace Final_Project_V2.Models
 {
     public class Song
     {
+        //scalar
+        public Int32 SongID { get; set; }
+        public string SongTitle { get; set; }
+        public DateTime SongTimestamp { get; set; }
+
+        //navigational
+        public virtual Artist SongArtist { get; set; }
+        public virtual Genre SongGenre { get; set; }
+        public virtual List<Album> SongAlbums { get; set; }
+        
     }
 }
