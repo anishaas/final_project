9203109f62d341df2223a09a256e95426bf474f6
anishaas <anisha.srivastava123@gmail.com>
2016-11-14 23:19:02 -0600

    added properties to album model class

--- Final_Project_V2/Models/Album.cs
+++ Final_Project_V2/Models/Album.cs
@@ -7,5 +7,16 @@ namespace Final_Project_V2.Models
 {
     public class Album
     {
+        //scalar 
+        public Int32 AlbumID { get; set; }
+        public decimal AlbumPrice { get; set; }
+        public string AlbumName { get; set; }
+        public DateTime AlbumTimestamp { get; set; }
+
+        //navigational
+        public virtual Genre AlbumGenre { get; set; }
+        public virtual Artist AlbumArtist { get; set; }
+        public virtual List<Song> AlbumSongs { get; set; }
+        
     }
 }
--- Final_Project_V2/Models/Genre.cs
+++ Final_Project_V2/Models/Genre.cs
@@ -7,5 +7,8 @@ namespace Final_Project_V2.Models
 {
     public class Genre
     {
+
+
+        public virtual List<Album> Albums { get; set; }
     }
 }
