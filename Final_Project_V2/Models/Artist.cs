457e18ffecaa52ce0c0e4509e618d7c5b8bc558d
anishasrivastava <anishasrivastava@ANISHASRIVAE94D>
2016-11-28 13:57:46 -0600

    Added constructor to Artist model class, seeding songs and albums successful

--- Final_Project_V2/Models/Artist.cs
+++ Final_Project_V2/Models/Artist.cs
@@ -8,6 +8,12 @@ namespace Final_Project_V2.Models
 {
     public class Artist
     {
+        //constructor
+        public Artist()
+        {
+           this.ArtistGenres = new List<Genre>();
+        }
+        
         public Int32 ArtistID { get; set; }
         public string ArtistName { get; set; }
        // public DateTime ArtistTimeStamp { get; set; }
