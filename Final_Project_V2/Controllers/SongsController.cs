f3b5fe830134994eb70006fae2dc55190e429ff1
anishasrivastava <anishasrivastava@ANISHASRIVAE94D>
2016-11-29 03:06:03 -0600

    Creating song genre selection functionality

--- Final_Project_V2/Controllers/SongsController.cs
+++ Final_Project_V2/Controllers/SongsController.cs
@@ -42,6 +42,7 @@ namespace Final_Project_V2.Controllers
         // GET: /Songs/Create
         public ActionResult Create()
         {
+            ViewBag.AllGenres= GetAllGenres();
             return View();
         }
 
@@ -62,6 +63,48 @@ namespace Final_Project_V2.Controllers
             //need to make sure that SongArtist, SongGenres, SongAlbums are added and are valid selections
             return View(song);
         }
+
+        public SelectList GetAllGenres() //NO COMMITTEE SELECTED
+        {
+            //create query to find all committees
+            var query = from c in db.Genres
+                        orderby c.GenreName
+                        select c;
+            //execute query and store in list
+            List<Genre> allGenres = query.ToList();
+
+            //convert list to selectlist format for HTML
+            SelectList allGenresList = new SelectList(allGenres, "GenreID", "GenreName");
+
+            return allGenresList;
+        }
+
+        public MultiSelectList GetAllGenres(Song @song)
+        {
+            //find the list of genres
+            var query = from m in db.Genres
+                        orderby m.GenreName
+                        select m;
+
+            //convert to list and execute query
+            List<Genre> allGenres = query.ToList();
+            //create list of selected members
+            List<Int32> SelectedGenres = new List<Int32>();
+
+            //Loop through list of genres and add GenreId
+            foreach (Genre g in @song.SongGenres)
+            {
+                int ID = g.GenreID;
+                SelectedGenres.Add(ID);
+            }
+
+            //convert to multiselect
+            MultiSelectList allGenresList = new MultiSelectList(allGenres, "GenreID", "GenreName",
+            SelectedGenres);
+
+            return allGenresList;
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
