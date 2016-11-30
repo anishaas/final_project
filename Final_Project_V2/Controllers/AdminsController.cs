7674043db711c511dce32561ffbab9dcc2b01fd9
anishasrivastava <anishasrivastava@ANISHASRIVAE94D>
2016-11-30 00:15:47 -0600

    Added a property to discount model class

---
+++ Final_Project_V2/Controllers/AdminsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Final_Project_V2.Models;
+
+namespace Final_Project_V2.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Admins
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Employees/ManageSongs
+        [Authorize(Roles = "Admin")]
+        public ActionResult ManageSongs()
+        {
+
+            var query = from s in db.Songs
+                        select s;
+            List<Song> allSongs = query.ToList();
+            return View(allSongs);
+        }
+    }
+}
