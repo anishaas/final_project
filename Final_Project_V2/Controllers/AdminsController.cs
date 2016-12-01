a3e908475e8a7d1a693d6995f726abd81e22a765
anishasrivastava <anishasrivastava@ANISHASRIVAE94D>
2016-11-30 21:49:22 -0600

    Creating songs works

--- Final_Project_V2/Controllers/AdminsController.cs
+++ Final_Project_V2/Controllers/AdminsController.cs
@@ -33,7 +33,7 @@ namespace Final_Project_V2.Controllers
         // POST: Admins/CreateSong
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "SongID,SongTitle,SongPrice,Featured")] Song @song, int[] SelectedGenres, int[] SelectedArtists)
+        public ActionResult CreateSong([Bind(Include = "SongID,SongTitle,SongPrice,Featured")] Song @song, int[] SelectedGenres, int[] SelectedArtists)
         {
             if (ModelState.IsValid)
             {
@@ -61,7 +61,7 @@ namespace Final_Project_V2.Controllers
                 return RedirectToAction("ManageSongs");
             }
 
-            return View("~/Views/SandBoxViews/WelcomePage/customerWelcomePage.cshtml");
+            return View();
         }
 
 
